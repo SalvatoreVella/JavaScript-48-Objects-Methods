3bd332507f1b548f7e9145ae750ba057591977cc
Salvatore Vella <salvatorevella1234@gmail.com>
2022-11-21 11:11:37 +0100

    Exercise correction

--- exercise-1.js
+++ exercise-1.js
@@ -4,13 +4,8 @@ const person = {
   age: 25
 }
 
-let keys = Object.keys(person);
-console.log(keys);
-
-let values = Object.values(person);
-console.log(values);
-
-let entries = Object.entries(person);
-console.log(entries);
+for (values of Object.keys(person)) {
+  console.log(`${values} ${person[values]}`);
+}
 
 // Print values of person using Object.keys
